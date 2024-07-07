8d926c439dd0a3374b44bca831c719ed81aa1147
adrianc <adrianc@nvidia.com>
2024-07-07 14:10:42 +0300

    Add MaintenanceOperatorConfig CRD skaffolding
    Signed-off-by: adrianc <adrianc@nvidia.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	maintenancev1alpha1 "github.com/Mellanox/maintenance-operator/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -69,6 +71,9 @@ var _ = BeforeSuite(func() {
 	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
+
+	err = maintenancev1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
 
