282c92111f22559dcd63533db6c2165787f9aa9d
adrianc <adrianc@nvidia.com>
2024-08-07 14:44:55 +0300

    k8utils updates
    - Add additional helper methods
    - Modify some existing functions to accept k8sclient
      and update object in api.
    
    Signed-off-by: adrianc <adrianc@nvidia.com>

--- internal/k8sutils/k8sutils.go
+++ internal/k8sutils/k8sutils.go
@@ -17,9 +17,14 @@
 package k8sutils
 
 import (
+	"context"
+	"slices"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	maintenancev1 "github.com/Mellanox/maintenance-operator/api/v1alpha1"
 )
@@ -34,26 +39,36 @@ func GetReadyConditionReason(nm *maintenancev1.NodeMaintenance) string {
 }
 
 // SetReadyConditionReason sets or updates Ready condition in nm.Status with reason.
-// returns true if conditions were updated in nm object or false otherwise.
-func SetReadyConditionReason(nm *maintenancev1.NodeMaintenance, reason string) (changed bool) {
-	return SetReadyConditionReasonMsg(nm, reason, "")
+// in addition, it updates status of the object in k8s api if required.
+// returns error if occurred.
+func SetReadyConditionReason(ctx context.Context, client client.Client, nm *maintenancev1.NodeMaintenance, reason string) error {
+	return SetReadyConditionReasonMsg(ctx, client, nm, reason, "")
 }
 
 // SetReadyConditionReasonMsg sets or updates Ready condition in nm.Status with reason and msg.
-// returns true if conditions were updated in nm object or false otherwise.
-func SetReadyConditionReasonMsg(nm *maintenancev1.NodeMaintenance, reason string, msg string) (changed bool) {
+// in addition, it updates status of the object in k8s api if required.
+// returns error if occurred.
+func SetReadyConditionReasonMsg(ctx context.Context, client client.Client, nm *maintenancev1.NodeMaintenance, reason string, msg string) error {
 	status := metav1.ConditionFalse
 	if reason == maintenancev1.ConditionReasonReady {
 		status = metav1.ConditionTrue
 	}
 
 	cond := metav1.Condition{
-		Type:    maintenancev1.ConditionTypeReady,
-		Status:  status,
-		Reason:  reason,
-		Message: msg,
+		Type:               maintenancev1.ConditionTypeReady,
+		Status:             status,
+		ObservedGeneration: nm.Generation,
+		Reason:             reason,
+		Message:            msg,
 	}
-	return meta.SetStatusCondition(&nm.Status.Conditions, cond)
+
+	changed := meta.SetStatusCondition(&nm.Status.Conditions, cond)
+	var err error
+	if changed {
+		err = client.Status().Update(ctx, nm)
+	}
+
+	return err
 }
 
 // IsUnderMaintenance returns true if NodeMaintenance is currently undergoing maintenance
@@ -79,4 +94,49 @@ func IsNodeReady(n *corev1.Node) bool {
 // IsNodeUnschedulable returns true if node is Unschedulabel
 func IsNodeUnschedulable(n *corev1.Node) bool {
 	return n.Spec.Unschedulable
+}
+
+// AddFinalizer conditionally adds finalizer from NodeMaintenance
+func AddFinalizer(ctx context.Context, k8sClient client.Client, nm *maintenancev1.NodeMaintenance, finalizer string) error {
+	instanceFinalizers := nm.GetFinalizers()
+	if !slices.Contains(instanceFinalizers, finalizer) {
+		nm.SetFinalizers(append(instanceFinalizers, finalizer))
+		if err := k8sClient.Update(ctx, nm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// RemoveFinalizer conditionally removes finalizer from NodeMaintenance
+func RemoveFinalizer(ctx context.Context, k8sClient client.Client, nm *maintenancev1.NodeMaintenance, finalizer string) error {
+	instanceFinalizers := nm.GetFinalizers()
+	i := slices.Index(instanceFinalizers, finalizer)
+	if i >= 0 {
+		newFinalizers := slices.Delete(instanceFinalizers, i, i+1)
+		nm.SetFinalizers(newFinalizers)
+		if err := k8sClient.Update(ctx, nm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetOwnerRef conditionally sets owner referece of object to owner
+func SetOwnerRef(ctx context.Context, k8sClient client.Client, owner metav1.Object, object client.Object) error {
+	err := controllerutil.SetOwnerReference(owner, object, k8sClient.Scheme())
+	if err != nil {
+		return err
+	}
+	return k8sClient.Update(ctx, object)
+}
+
+// HasOwnerRef returns true if object owned by owner
+func HasOwnerRef(owner metav1.Object, object metav1.Object) bool {
+	for _, o := range object.GetOwnerReferences() {
+		if o.UID == owner.GetUID() {
+			return true
+		}
+	}
+	return false
 }
