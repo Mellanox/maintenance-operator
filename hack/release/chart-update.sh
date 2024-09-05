02944f5a1403434056e4f9ee5e271eef668e2451
adrianc <adrianc@nvidia.com>
2024-09-05 08:38:29 +0300

    fix image repo in helm chart update
    align repo name to be lowercase
    
    Signed-off-by: adrianc <adrianc@nvidia.com>

--- hack/release/chart-update.sh
+++ hack/release/chart-update.sh
@@ -3,7 +3,7 @@ set -ex
 
 # github tag e.g v1.2.3
 GITHUB_TAG=${GITHUB_TAG:-}
-# github repo owner e.g mellanox
+# github repo owner e.g Mellanox
 GITHUB_REPO_OWNER=${GITHUB_REPO_OWNER:-}
 
 BASE=${PWD}
@@ -28,7 +28,7 @@ OPERATOR_TAG=${GITHUB_TAG}
 # patch values.yaml in-place
 
 # maintenance-operator image:
-OPERATOR_REPO=${GITHUB_REPO_OWNER} # this is used to allow to release maintenance-operator from forks
+OPERATOR_REPO=$(echo ${GITHUB_REPO_OWNER} | tr '[:upper:]' '[:lower:]') # this is used to allow to release maintenance-operator from forks
 $YQ_CMD -i ".operator.image.repository = \"ghcr.io/${OPERATOR_REPO}/maintenance-operator\"" ${HELM_VALUES}
 
 # patch Chart.yaml in-place
